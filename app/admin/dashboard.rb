277785fde5555bf3bafabd6f2227712218b43061
grimmwerks <grimm@grimmwerks.com>
2016-09-20 11:22:27 -0400

    john says change added total to 360 rather than 540

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -10,7 +10,7 @@ ActiveAdmin.register_page "Dashboard" do
       column do
         Poll.all.includes(:poll_questions, :poll_answers).each do |p|
           panel link_to("Poll: #{p.title}   -   Total Votes: #{p.chicago_votes.count}", edit_admin_poll_path(p)) do
-            h3 "Image Downloads: #{ChicagoVote.where(voteable_type: "PollScene").count + 540}"
+            h3 "Image Downloads: #{ChicagoVote.where(voteable_type: "PollScene").count + 360}"
             p.poll_questions.each do |pq|
               panel pq.title do
                 table_for pq.poll_answers.each do |pa|
